9738b24067027df05e93c599e3baa17082fb16a5
minhduc <minhduc1994vcu@gmail.com>
2019-04-30 18:13:58 +0700

    seaech a student's name in an array

--- src/main/java/FindValue.java
+++ src/main/java/FindValue.java
@@ -1,3 +1,22 @@
-public class FindValue {
+import java.util.Scanner;
 
+public class FindValue {
+    public static void main(String[] args) {
+        String[] students = {"thanh", "nam", "son", "minh", "dang", "hung"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a student's name: ");
+        String name = scanner.nextLine();
+        boolean wasFound = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(name)) {
+                wasFound = true;
+                System.out.printf("%s was found at position %d", name, i + 1);
+                break;
+            }
+        }
+        if (!wasFound) {
+            System.out.println(name + " was not found");
+
+        }
+    }
 }
